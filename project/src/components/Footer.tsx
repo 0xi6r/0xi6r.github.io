40d2ca0167bf7019d2b6172e4e9b832ec19fc589
Isaac <193503195+0xi6r@users.noreply.github.com>
2025-07-24 15:24:19 +0300

    Update Footer.tsx

--- project/src/components/Footer.tsx
+++ project/src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           {/* Brand */}
           <div>
             <div className="flex items-center space-x-3 mb-4">
-              <img src='../0xi6r.svg' className="text-cyan-400 w-8 h-8" />
+              <img src='../0xi6r.png.png' className="text-cyan-400 w-8 h-8" />
               <span className="text-white font-bold text-xl">0xi6r</span>
             </div>
             <p className="text-gray-400 mb-4">
