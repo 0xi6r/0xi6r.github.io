f7b2da7eaaae5f880cb18c3aef569d46e32e377b
Isaac <193503195+0xi6r@users.noreply.github.com>
2025-07-28 15:24:55 +0300

    Update supabase.tsx

--- project/src/lib/supabase.tsx
+++ project/src/lib/supabase.tsx
@@ -1,7 +1,7 @@
 // lib/supabase.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "data"
-const supabaseAnonKey = "data"
+const supabaseUrl = "https://tobeupdatedsoon.supabase.co"
+const supabaseAnonKey = "siteisstillbeingbuildt.eyJpc3M siteisstillunderdevelopment.underconstruction"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
