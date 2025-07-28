987a72acc24b09f4d56b59c0c058986f7b5d8fb2
root <root@lsd.segfault.net>
2025-07-28 10:27:13 +0000

    app navigation update

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
+import Terms from './pages/Terms';
+import RedTeamServices from './pages/RedTeamServices';
+import SecurityConsulting from './pages/SecurityConsulting';
+import PenetrationTesting from './pages/PenetrationTesting';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path='/Terms' element={<Terms />} />
+          <Route path='/RedTeamServices' element={<RedTeamServices />} />
+          <Route path='/SecurityConsulting' element={<SecurityConsulting />} />
+          <Route path='PenetrationTesting' element ={<PenetrationTesting />} />
+          <Route path='*' element={<NotFound />} />
+
         </Routes>
       </Layout>
     </Router>
