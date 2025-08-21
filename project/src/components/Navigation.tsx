d671b6f6db2bfa59604829e0090806b29eeeacd0
Isaac <193503195+0xi6r@users.noreply.github.com>
2025-08-21 18:37:58 +0300

    Update Navigation.tsx
    clean up remove consulting page

--- project/src/components/Navigation.tsx
+++ project/src/components/Navigation.tsx
@@ -4,7 +4,6 @@ import { Link, useLocation } from 'react-router-dom';
 
 const services = [
   { name: "Penetration Testing", href: "/PenetrationTesting" },
-  { name: "Security Consulting", href: "/SecurityConsulting" },
   { name: "Red Team Operations", href: "/RedTeamServices" },
   { name: "System Administration & Threat Intelligence", href: "/SysAdminServices" }
 ];
