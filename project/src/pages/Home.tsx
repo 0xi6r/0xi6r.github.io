8fef3489365171b57b51403934623daa51e6c49f
Isaac <193503195+0xi6r@users.noreply.github.com>
2026-02-03 16:56:52 +0300

    Update import path for ProjectShowcase component

--- project/src/pages/Home.tsx
+++ project/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import ProjectShowcase from "../components/ProjectShowcase";
+import ProjectShowcase from "@/components/ProjectShowcase";
 import { 
   Calendar, 
   ArrowRight, 
