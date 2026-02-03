a08a75e12423fc799eea49d7553c46132957b990
Isaac <193503195+0xi6r@users.noreply.github.com>
2026-02-03 16:33:05 +0300

    Add ProjectShowcase component to Home page

--- project/src/pages/Home.tsx
+++ project/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import ProjectShowcase from "../components/ProjectShowcase";
 import { 
   Calendar, 
   ArrowRight, 
@@ -302,6 +303,9 @@ const HomePage: React.FC = () => {
           )}
         </div>
       </section>
+        <div>
+          <ProjectShowCase />
+        </div>
     </div>
   );
 };
