3547382b40b8938541c84df6c2498d11daaf7a56
Isaac <193503195+0xi6r@users.noreply.github.com>
2026-02-06 22:41:02 +0300

    Fix import statement for GitHubShowcase component

--- project/src/pages/About.tsx
+++ project/src/pages/About.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Shield, Award, Users, Clock, ArrowRight } from 'lucide-react';
-import { GithubShowcase } from './GitHubShowcase';
+import { GitHubShowcase } from './GitHubShowcase';
 
 const About = () => {
   const stats = [
