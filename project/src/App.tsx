48c0f902308fa3a1809fb4d7cfc64253286cdbb5
Isaac <193503195+0xi6r@users.noreply.github.com>
2026-02-06 22:51:02 +0300

    Add GitHubShowcase import to App component

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -10,6 +10,7 @@ import Terms from './pages/Terms';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import VulnResearch from './pages/VulnResearch';
 import Services from './pages/Services';
+import GitHubShowcase from './pages/GitHubShowcase';
 import NotFound from './pages/NotFound';
 
 function App() {
