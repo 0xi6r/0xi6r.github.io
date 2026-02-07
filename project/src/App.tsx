005b80b2a0f8591409a7afd8e3daf2c77dae7b48
Isaac <193503195+0xi6r@users.noreply.github.com>
2026-02-08 01:01:19 +0300

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -11,6 +11,7 @@ import PrivacyPolicy from './pages/PrivacyPolicy';
 import VulnResearch from './pages/VulnResearch';
 import Services from './pages/Services';
 import GitHubShowcase from './components/GitHubShowcase';
+import VulnerabilityResearchPage from './pages/VulnerabilityResearchPag';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -25,7 +26,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path='/Terms' element={<Terms />} />
           <Route path='/PrivacyPolicy' element={<PrivacyPolicy />} />
-          <Route path='/VulnResearch' element={<VulnResearch />} />
+          <Route path='/VulnerabilityResearchPage' element={<VulnerabilityResearchPage />} />
           <Route path='/Services' element={<Services />} />
           <Route path='*' element={<NotFound />} />
 
