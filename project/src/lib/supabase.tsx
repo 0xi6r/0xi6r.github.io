fb7f8398266f4dd5905c807c2fca3560e53fbcde
0xi6r <193503195+0xi6r@users.noreply.github.com>
2026-02-24 22:32:36 +0300

    Delete project/src/lib/supabase.tsx
    placeholder for future supabase integration has to go as we shift to an altenative solution.

--- project/src/lib/supabase.tsx
+++
@@ -1,7 +0,0 @@
-// lib/supabase.ts
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = "https://tobeupdatedsoon.supabase.co"
-const supabaseAnonKey = "siteisstillbeingbuildt.eyJpc3M siteisstillunderdevelopment.underconstruction"
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
