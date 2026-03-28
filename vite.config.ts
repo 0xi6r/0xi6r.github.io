19c0e2c8d099d736dd64a6a79588cee5d16dfab2
0xi6r <193503195+0xi6r@users.noreply.github.com>
2026-03-29 00:49:24 +0300

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/0xi6r.github.io/",
+  base: "/",
   plugins: [react()],
   assetsInclude: ['**/*.md'],
   resolve: {
