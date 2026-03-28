e47675e90456cf18aaca8d06a2d8c807102bc078
0xi6r <193503195+0xi6r@users.noreply.github.com>
2026-03-29 00:38:47 +0300

    Set base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/0xi6r.github.io/",
   plugins: [react()],
   assetsInclude: ['**/*.md'],
   resolve: {
